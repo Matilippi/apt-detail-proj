d645805ba6077bdfe88c03ae0a1587744c668887
Matilde Lippi <matilde.lippi94@gmail.com>
2023-11-28 22:57:59 +0100

    rework test class + pitclipse plugin added

--- com.detail.app.mydetail/src/test/java/com/detail/app/mydetail/SurgeryTest.java
+++ com.detail.app.mydetail/src/test/java/com/detail/app/mydetail/SurgeryTest.java
@@ -1,46 +1,47 @@
 package com.detail.app.mydetail;
 
 import static org.assertj.core.api.Assertions.*;
+
+import org.junit.Before;
 import org.junit.Test;
 
 
 public class SurgeryTest {
+	private Surgery surgery1 = new Surgery("Paziente1");
+	private Surgery surgery2 = new Surgery("Paziente2");
+	
+	@Before
+	public void setup() {
+		surgery1 = new Surgery("Paziente1");
+		surgery2 = new Surgery("Paziente2");
+	}
 
 	@Test
 	public void testIsAutomaticallyAssignPositiveId() {
-		Surgery surgery = new Surgery("Paziente1");
-		assertThat(surgery.getId()).isPositive();
+		assertThat(surgery1.getId()).isPositive();
 	}
 	
 	@Test
 	public void testIdAreIncremental() {
-		Surgery surgery1 = new Surgery("Paziente1");
-		Surgery surgery2 = new Surgery("Paziente2");
-
 		assertThat(surgery2.getId()).isGreaterThan(surgery1.getId());
 	}
 	
 	@Test
 	public void testNameIsCorrectlyAssign() {
-		Surgery surgery1 = new Surgery("Paziente1");
-
 		assertThat(surgery1.getPatientName()).isEqualTo("Paziente1");
 	}
 	
 	@Test
 	public void testJouleIsCorrectlyCalculated() {
-		Surgery surgery = new Surgery("Paziente1");
-		surgery.pressButton(3);
+		surgery1.pressButton(3);
 
-		assertThat(surgery.getJouleUsed()).isEqualTo(9);
+		assertThat(surgery1.getJouleUsed()).isEqualTo(9);
 	}
 	
 	@Test
 	public void testIfSecondsNegativeShouldThrow() {
-		
-		Surgery surgery = new Surgery("Paziente1");
 		assertThatThrownBy(()->
-		surgery.pressButton(-3))
+		surgery1.pressButton(-3))
 			.isInstanceOf(IllegalArgumentException.class)
 			.hasMessage("Negative seconds passed: -3");
 	}
