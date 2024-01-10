913f78b475d470c3c9f98e6a6171ea39dea7f618
Matilde Lippi <matilde.lippi94@gmail.com>
2024-01-10 11:57:56 +0100

    fix docker action

--- com.detail.app.mydetail/src/main/java/com/detail/app/mydetail/App.java
+++ com.detail.app.mydetail/src/main/java/com/detail/app/mydetail/App.java
@@ -2,6 +2,6 @@ package com.detail.app.mydetail;
 
 public class App {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Hello Docker!");
 	}
 }
